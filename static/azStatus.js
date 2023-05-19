7f111eca6308f6613214295c8da798818272c735
lhQ <lh@q>
2023-05-19 10:59:51 +0200

    before adding new rows, delete all existing. show spinner while loading

--- static/azStatus.js
+++ static/azStatus.js
@@ -1,8 +1,8 @@
 
 function addRow(vm) {
-	let table = document.getElementById('vmTable');
-	let row = table.insertRow();
-	row.id = 'vmTableRow' + table.rows.length;
+	let body = document.getElementById('vmTableBody');
+	let row = body.insertRow();
+	row.id = 'vmTableRow' + body.rows.length;
 
 	let cell1 = row.insertCell(0);
 	cell1.innerHTML = vm.name;
@@ -50,11 +50,17 @@ function loadVMs() {
 		} else {
 			e.json().then(function(respJson) {
 				let vmArray = respJson.status;
+
+				// clear table before adding new rows
+				let body = document.getElementById('vmTableBody');
+				for (let i=body.rows.length-1; i>=0; i--) {
+					body.deleteRow(i)
+				}
+
 				for (i in vmArray) {
 					addRow(vmArray[i])
 				}
 
-				//let startButtons = document.getElementsByClassName('btnStartVM');
 				let startButtons = document.querySelectorAll('.btnStartVM:not(disabled)');
 				for (let i=0; i<startButtons.length; i++) {
 					startButtons[i].addEventListener('click', function() {
@@ -79,6 +85,8 @@ function loadVMs() {
 				}
 			});
 		}
+
+		showHideSpinner(false)
 	});
 }
 
@@ -86,18 +94,29 @@ function changeVmStatus(action, rowId) {
 	let vmName = document.getElementById(rowId).querySelectorAll('.vmName')[0].innerHTML
 	let rg = document.getElementById(rowId).querySelectorAll('.vmRg')[0].innerHTML
 
+	showHideSpinner(true)
 	fetch(`/${action}?rg=${encodeURIComponent(rg)}&vm=${encodeURIComponent(vmName)}`, {
 		method: 'POST',
 	}).then(function(response) {
 		console.log(response);
 		if (response.status == 200) {
-			// XXX do sth
-			console.log('success')
+			loadVMs()
+			showHideSpinner(false)
 		} else {
-			// XXX proper fancy dialog
+			showHideSpinner(false)
+			loadVMs()
 			alert(response.statusText);
 		}
 	});
+}
+
+function showHideSpinner(show) {
+	let spinner = document.getElementById('spinner');
+	if (show) {
+		spinner.style.visibility = 'visible'
+	} else {
+		spinner.style.visibility = 'hidden'
+	}
 }
 
 (function() {
